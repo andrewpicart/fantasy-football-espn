e55c1b89a31a41b6082bcf84149d9d3634cfaa5f
unknown <andrew.picart@gmail.com>
2019-11-28 04:27:08 -0500

    ignore keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-# keys are here
-# enter the keys for your league here
-
-league_id = 140547
-swid = '{2CF7EB07-52CE-4A77-B5CF-93261DF48AB1}'
-espn_s2 = 'AEBnBUS4C%2FEQt6V8HTh9HgHbGP3KC4rc4rdspzt3JA8wO5r9i7p8%2BYbqnTyEgLEFT6Qrd9DXKfxjOkhXPC6DeMKKxW%2BO16DgudBu3cMe6avfjogteybWbtRk7aTe%2FIHbftKlsNZ2XTBkVFdCuXriaZjpTBeLFxdXLhfkmYreo6EhtAKzSn0qWfEZhsmdcF4wGOqeLxYXXJSLCQIVJcffhaSie6lhv2NVK1KbCDvkW1Q468GTXHkmED4hsneBBoI0o5s%3D'
